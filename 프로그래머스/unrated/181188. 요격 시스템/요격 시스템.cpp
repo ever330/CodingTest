2a4b443468b720dd9f86057c88dfbf6600ced939
ever330 <79270240+ever330@users.noreply.github.com>
2023-04-28 15:23:18 +0900

    [unrated] Title: 요격 시스템, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool compare(vector<int> a, vector<int> b)
+{
+	return a[1] < b[1];
+}
+
+int solution(vector<vector<int>> targets)
+{
+	int answer = 0;
+
+	sort(targets.begin(), targets.end(), compare);
+
+	int nowTarget = -1;
+
+	for (auto target : targets)
+	{
+		if (target[0] >= nowTarget)
+		{
+			answer++;
+			nowTarget = target[1];
+		}
+	}
+
+	return answer;
+}
