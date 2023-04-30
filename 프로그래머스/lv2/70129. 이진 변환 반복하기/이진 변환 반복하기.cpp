64e2b22e37518fc77db435ed23217e6e20c841fe
ever330 <79270240+ever330@users.noreply.github.com>
2023-04-30 17:55:52 +0900

    Update 이진 변환 반복하기.cpp

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.cpp
@@ -3,7 +3,46 @@
 
 using namespace std;
 
-vector<int> solution(string s) {
-    vector<int> answer;
+
+string DecToBin(int num)
+{
+    string binNum;
+
+    while (num != 1)
+    {
+        binNum.insert(0, to_string(num % 2));
+        num /= 2;
+    }
+    binNum.insert(0, to_string(num));
+
+    return binNum;
+}
+
+vector<int> solution(string s) 
+{
+    vector<int> answer = { 0, 0 };
+
+    vector<int> eraseNum;
+
+    while (s.size() != 1)
+    {
+        eraseNum.clear();
+        for (auto i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '0')
+            {
+                eraseNum.push_back(i);
+            }
+        }
+        for (auto i = eraseNum.size(); i > 0; i--)
+        {
+            s.erase(s.begin() + eraseNum[i - 1]);
+            answer[1]++;
+        }
+
+        answer[0]++;
+        s = DecToBin(s.size());
+    }
+
     return answer;
 }
